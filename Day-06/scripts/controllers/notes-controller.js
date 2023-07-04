3e48a0fa8cbebad0ba1d8e22b7141fbd74a316ce
yashyvs <yashvardhan.yvs21@gmail.com>
2023-07-05 03:23:04 +0530

    add css

--- Day-06/scripts/controllers/notes-controller.js
+++ Day-06/scripts/controllers/notes-controller.js
@@ -1,11 +1,4 @@
 function addNotes() {
-  // read id, title, desc, completion date, importance
-  // const id = document.querySelector('#id').value;
-  // const title = document.querySelector('#title').value;
-  // const desc = document.querySelector('#desc').value;
-  // const cdate = document.querySelector("#cdate").value;
-  // const importance = document.querySelector("#importance").value;
-
   const fields = ["id", "title", "desc", "cdate", "importance"];
   const noteObject = {}; // obj literal
   for (let field of fields) {
@@ -21,4 +14,38 @@ function printNote(noteObject) {
     const td = row.insertCell();
     td.innerText = noteObject[key];
   }
+}
+
+function deleteNotes() {
+  const tbody = document.querySelector("#notes");
+  const rowCount = tbody.rows.length;
+
+  if (rowCount >= 1) {
+    tbody.deleteRow(rowCount - 1);
+  }
+}
+
+function sortNotes() {
+  const tbody = document.querySelector("#notes");
+  const rows = Array.from(tbody.rows);
+  rows.sort((rowA, rowB) => {
+    const idA = parseInt(rowA.cells[0].innerText);
+    const idB = parseInt(rowB.cells[0].innerText);
+    return idA - idB;
+  });
+
+  while (tbody.firstChild) {
+    tbody.removeChild(tbody.firstChild);
+  }
+
+  rows.forEach((row) => {
+    tbody.appendChild(row);
+  });
+}
+
+function clearNotes() {
+  const tbody = document.querySelector("#notes");
+   while (tbody.firstChild) {
+     tbody.removeChild(tbody.firstChild);
+   }
 }
